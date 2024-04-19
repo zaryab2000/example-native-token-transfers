bcd99bd4af124356cffd99d22a9a4f3774fa16d4
Zaryab <zaryabafser2000@gmail.com>
2024-04-19 05:18:39 +0400

    updated Interface

--- evm/src/BridgeDemo/IPayloadSender.sol
+++ evm/src/BridgeDemo/IPayloadSender.sol
@@ -6,9 +6,11 @@ interface IPayloadSender {
     /// @param _to The address to receive the minted tokens.
     /// @param _amount The amount of tokens to mint.
     function sendPushTokensOnly(address _to, uint256 _amount) external payable;
+    function sendPushTokenWithMSG(address _to, uint256 _amount, string memory _msg, address targetContractAddress) external payable;
+
     function version() external view returns(uint256);
     function SEPOLIA_TOKEN() external view returns(address);
-
+    function latestGreeting() external view returns(string memory);
 
 
 }
